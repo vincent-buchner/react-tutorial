a498811dbfa0afbf40364d72372bb11041238f53
Vincent Buchner <vincentbuchner01@gmail.com>
2022-03-23 13:34:28 -0500

    Added keys to the list of objects

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const lakeList = ["Big Lake", "Small Lake", "Deutsch Lake"];
+const lakeList = [
+  {id: "1", name:"Small Lake", trialhead:"Young Buck"},
+  {id: "2", name:"Large Lake", trialhead:"Papa Bear"},
+  {id: "3", name:"Deutsch See", trialhead:"Brot mit Käse"}
+];
 
 function App({lakes}) {
   return(
     // For each lake in the array, put it inside that JSX code
-    <ul>
-      {lakes.map(lake => <li>{lake}</li>)}
-    </ul>
+    <div>
+      {lakes.map(lake => 
+      <div key={lakes.id}>
+        <h2>{lake.name}</h2>
+        <p>Access Point: {lake.trialhead}</p>
+      </div>)}
+    </div>
   )
 }
 
