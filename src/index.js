19b67209363cdd19bc62b918ed7a22d1db027ab3
Vincent Buchner <vincentbuchner01@gmail.com>
2022-03-23 21:48:24 -0500

    More examples of useEffect

--- src/index.js
+++ src/index.js
@@ -2,28 +2,32 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Checkbox() {
-  const [checked, setChecked] = useState(false);
-
-  useEffect(() => {
-    console.log(`checked: ${checked.toString()}`)
-  })
-  
-  return(
-    <>
-      <input 
-        type="checkbox" 
-        value={checked}
-        onChange={() => setChecked(checked => !checked)}
-      />
-      {/* IF the variable checked is true, write "checked". Otherwise, write "Not Checked"  */}
-      {checked ? "checked" : "Not Checked"}
-    </>
-  );
-}
 
 function App() {
-  return <Checkbox />
+  const [val, setVal] = useState("")
+  const [val2, setVal2] = useState("")
+
+  useEffect(() => {
+    console.log(`Field 1: ${val}`)
+  }, [val])
+
+  useEffect(() => {
+    console.log(`Field 2: ${val2}`)
+  }, [val2])
+
+  return(
+    <>
+      <label>
+        <h1>Favorite Phrase</h1>
+        <input value={val} onChange={change => setVal(change.target.value)}/>
+      </label>
+      <br />
+      <label>
+        <h1>Second Favorite Phrase</h1>
+        <input value={val2} onChange={change => setVal2(change.target.value)}/>
+      </label>
+    </>
+  );
 }
 
 ReactDOM.render(
